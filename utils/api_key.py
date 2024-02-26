1cc718b7b0464e4d339df0a8f7fc88619201af54
Weiyue Li <wel019@ucsd.edu>
2024-02-25 21:23:14 -0800

    push website

--- utils/api_key.py
+++
@@ -1,7 +0,0 @@
-import os
-
-# You can either set `OPENAI_API_KEY` environment variable or replace "YOUR_API_KEY" below with your OpenAI API key
-if "OPENAI_API_KEY" in os.environ:
-    api_key = os.environ["OPENAI_API_KEY"]
-else:
-    api_key = "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"
