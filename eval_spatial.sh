ee5c57ece49b82720111cd66fa8933b2d349928f
Weiyue Li <wel019@ucsd.edu>
2024-03-08 05:28:10 -0800

    update eval_script and vis notebook

--- eval_spatial.sh
+++ eval_spatial.sh
@@ -1,16 +1,16 @@
-python evaluate_images.py --template_version v0.1 --yolo_model yolov8m --lm gpt-3.5 --detection True
-# python evaluate_images.py --template_version v0.4 --yolo_model yolov8m --lm gpt-3.5 --detection True
+# python evaluate_images.py --template_version v0.1 --yolo_model yolov8m --lm gpt-3.5 #--detection True
+# python evaluate_images.py --template_version v0.4 --yolo_model yolov8m --lm gpt-3.5 #--detection True
 # python evaluate_images.py --template_version v0.7 --yolo_model yolov8m
 # python evaluate_images.py --template_version v0.9 --yolo_model yolov8m
 
-python evaluate_images.py --template_version v0.1 --yolo_model yolov8x --lm gpt-3.5 --detection True
-# python evaluate_images.py --template_version v0.4 --yolo_model yolov8x --lm gpt-3.5 --detection True
+# python evaluate_images.py --template_version v0.1 --yolo_model yolov8x --lm gpt-3.5 #--detection True
+# python evaluate_images.py --template_version v0.4 --yolo_model yolov8x --lm gpt-3.5 #--detection True
 # python evaluate_images.py --template_version v0.7 --yolo_model yolov8x
 # python evaluate_images.py --template_version v0.9 --yolo_model yolov8x
 
 
-# python evaluate_images.py --template_version v0.1 --yolo_model yolov9e --lm gpt-3.5 --detection True
-# python evaluate_images.py --template_version v0.4 --yolo_model yolov9e --lm gpt-3.5 --detection True
+# python evaluate_images.py --template_version v0.1 --yolo_model yolov9e --lm gpt-3.5 #--detection True
+# python evaluate_images.py --template_version v0.4 --yolo_model yolov9e --lm gpt-3.5 #--detection True
 # python evaluate_images.py --template_version v0.7 --yolo_model yolov9e
 # python evaluate_images.py --template_version v0.9 --yolo_model yolov9e
 
@@ -24,4 +24,23 @@ python evaluate_images.py --template_version v0.1 --yolo_model yolov8x --lm gpt-
 # python evaluate_images.py --model_type sdxl --yolo_model yolov8x
 # python evaluate_images.py --model_type sdxl --yolo_model yolov9e
 
+
+python evaluate_images.py --sdxl False --template_version v0.1 --yolo_model yolov8m --lm gpt-4 --detection True
+python evaluate_images.py --sdxl False --template_version v0.4 --yolo_model yolov8m --lm gpt-4 --detection True
+python evaluate_images.py --sdxl False --template_version v0.7 --yolo_model yolov8m --lm gpt-4 --detection True
+python evaluate_images.py --sdxl False --template_version v0.9 --yolo_model yolov8m --lm gpt-4 --detection True
+python evaluate_images.py --sdxl False --template_version v0.1 --yolo_model yolov8m --lm gpt-3.5 --detection True
+
+python evaluate_images.py --sdxl False --template_version v0.1 --yolo_model yolov8x --lm gpt-4 --detection True
+python evaluate_images.py --sdxl False --template_version v0.4 --yolo_model yolov8x --lm gpt-4 --detection True
+python evaluate_images.py --sdxl False --template_version v0.7 --yolo_model yolov8x --lm gpt-4 --detection True
+python evaluate_images.py --sdxl False --template_version v0.9 --yolo_model yolov8x --lm gpt-4 --detection True
+python evaluate_images.py --sdxl False --template_version v0.1 --yolo_model yolov8x --lm gpt-3.5 --detection True
+
+
+python evaluate_images.py --sdxl False --template_version v0.1 --yolo_model yolov9e --lm gpt-4 --detection True
+python evaluate_images.py --sdxl False --template_version v0.4 --yolo_model yolov9e --lm gpt-4 --detection True
+python evaluate_images.py --sdxl False --template_version v0.7 --yolo_model yolov9e --lm gpt-4 --detection True
+python evaluate_images.py --sdxl False --template_version v0.9 --yolo_model yolov9e --lm gpt-4 --detection True
+python evaluate_images.py --sdxl False --template_version v0.1 --yolo_model yolov9e --lm gpt-3.5 --detection True
 
